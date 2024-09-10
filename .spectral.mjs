80e85a7a9fb520ee0704264c0643fbc733410de6
andres.olave@velocitycareerlabs.com <andres.olave@velocitycareerlabs.com>
2024-09-10 19:42:36 +1000

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NzU5OQ/spectral.js?branch_slug=main&token=28c61c03-8b01-4af8-9f93-26dc5846485e";
+export default { extends: ruleset };
